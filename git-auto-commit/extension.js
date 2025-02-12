fc89aa625c81439f3dc36aa2ef9e78b829a5b4f5
xander hayhoe <xanderhayhoe@gmail.com>
2025-02-12 14:02:10 -0500

    auto staged commit. To modify my message, please run: git commit --amend -m '<your message>'

--- git-auto-commit/extension.js
+++ git-auto-commit/extension.js
@@ -1,36 +1,43 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+const vscode = require("vscode");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  console.log(
+    'Congratulations, your extension "git-auto-commit" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "git-auto-commit" is now active!');
+  let disposable = vscode.commands.registerCommand(
+    "git-auto-commit.autoCommit",
+    function () {
+      const workspaceFolders = vscode.workspace.workspaceFolders;
+      if (!workspaceFolders || workspaceFolders.length === 0) {
+        vscode.window.showErrorMessage("No workspace is open");
+        return;
+      }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('git-auto-commit.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+      const workspaceRoot = workspaceFolders[0].uri.fsPath;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Git-Auto-Commit!');
-	});
+      const terminal = vscode.window.createTerminal("Git Auto Commit");
 
-	context.subscriptions.push(disposable);
+      terminal.sendText(`cd "${workspaceRoot}"`);
+
+      terminal.sendText("git add .");
+      terminal.sendText(
+        "git commit -m \"auto staged commit. To modify my message, please run: git commit --amend -m '<your message>'\""
+      );
+
+      terminal.show();
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
